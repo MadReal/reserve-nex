ec69e2ee78b67c83d1e0d12ed02de17d3c9802cb
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-17 10:10:04 +0200

    things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	devtools: { enabled: true },
+	modules: [
+		"@nuxt/image",
+		[
+			"@nuxtjs/google-fonts",
+			{
+				families: { Poppins: [300, 400, 500, 600] },
+				// preload: true,
+				download: true,
+				overwriting: false,
+				display: "swap",
+			},
+		],
+	],
+	css: ["~/assets/css/main.sass"],
+	image: { provider: "netlify", format: ["webp", "jpg"] },
+});
