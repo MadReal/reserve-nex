c8786f7793863ad5524e465784fc4c50690ec505
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-19 16:15:59 +0200

    page prenotazioni

--- pages/prenotazioni.vue
+++ pages/prenotazioni.vue
@@ -4,35 +4,42 @@ import reservations from "@/data/reservations.json";
 const formatDateAndTime = (isoTimestamp: string) => {
     const dateObject = new Date(isoTimestamp);
 
-    // Extracting the date
-    const year = dateObject.getUTCFullYear();
-    const month = dateObject.getUTCMonth() + 1;
-    const day = dateObject.getUTCDate();
-    const extractedDate = `${year}-${month}-${day}`;
+    // Format the date and time
+    const formattedDate = dateObject.toLocaleString("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
 
-    // Extracting the time
-    const hours = dateObject.getUTCHours();
-    const minutes = dateObject.getUTCMinutes();
-    const seconds = dateObject.getUTCSeconds();
-    const milliseconds = dateObject.getUTCMilliseconds();
-    const extractedTime = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    const formattedTime = dateObject.toLocaleString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false // Use 24-hour format
+    });
 
-    return { extractedDate, extractedTime };
+    return { formattedDate, formattedTime };
 };
 </script>
 
 <template lang="pug">
 .page
     PageTitle(title="Prenotazioni")
-    div.flex.items-center.py-4.border-b(v-for="item in reservations", :key="item.id")
-        p {{ item.name }}
-        p [# {{ item.id }}]
-        .flex
-            SVGIcon.text-primary-100(svg="users-filled", :size="20")
-            p {{ formatDateAndTime(item.date).extractedDate }} <!-- Display extracted date -->
 
-        .flex
-            SVGIcon.text-grey-100(svg="clock", :size="20")
-            p {{ formatDateAndTime(item.date).extractedTime }} <!-- Display extracted date -->
+    .flex.items-center.gap-5.py-4.border-b.font-medium(v-for="item in reservations", :key="item.id")
+        p.inline
+            span.pr-1.text-black.font-semibold {{ item.name }}
+            span.text-grey-200 {{ `[#${item.id}]` }}
+        p.antialiased {{ formatDateAndTime(item.date).formattedDate }}
+        .flex.items-center.text-primary-100.antialiased
+            SVGIcon.pr-1(svg="users-filled", :size="20")
+            p {{ item.people }}
+        .flex.items-center.text-grey-100.antialiased
+            SVGIcon.pr-1(svg="clock", :size="20")
+            p {{ formatDateAndTime(item.date).formattedTime }}
 
+        .ml-auto.flex.gap-2
+            a.py-1.px-3.rounded.bg-success-100.text-success-300(v-if="item.accepted === true") Accettata
+            a.py-1.px-3.rounded.bg-error-100.text-error-300(v-if="item.accepted === false") Rifiutata
+            a.py-1.px-3.rounded.bg-success-200.text-white(v-if="item.accepted === null") Accetta
+            a.py-1.px-3.rounded.bg-error-200.text-white(v-if="item.accepted === null") Rifiuta
 </template>
