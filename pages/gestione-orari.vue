07dd5f4769f0a064dfeec00c33d7660f06100d01
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-20 23:49:02 +0200

    gestione orari

--- pages/gestione-orari.vue
+++ pages/gestione-orari.vue
@@ -2,12 +2,24 @@
 import workHoursRestaurant from "@/data/work-hours-restaurant.json";
 import workHoursAvailable from "@/data/work-hours-available.json";
 
+// Define a constant for the imported data
+const restaurantPranzoHours = ref(workHoursRestaurant.pranzo);
 // pranzo
 let showPranzoAddTime = ref(true)
 let showPranzoSelect = ref(false)
-let isOpenPranzoDropdown = ref(true)
+let isPranzoDropdownOpen = ref(true)
 const togglePranzoSelect = () => { showPranzoAddTime.value = false; showPranzoSelect.value = true }
-const togglePranzoDropdown = () => isOpenPranzoDropdown.value = !isOpenPranzoDropdown.value;
+const togglePranzoDropdown = () => isPranzoDropdownOpen.value = !isPranzoDropdownOpen.value;
+const addTimeSetPranzo = (newTime: string) => {
+    restaurantPranzoHours.value.push(newTime)
+    restaurantPranzoHours.value.sort((a, b) => parseInt(a) - parseInt(b));
+    showPranzoAddTime.value = true
+    showPranzoSelect.value = false
+    isPranzoDropdownOpen.value = false
+}
+const removeTimeSetPranzo = (index: number) => restaurantPranzoHours.value.splice(index, 1);
+const isTimeUsed = (time: string) => workHoursRestaurant.pranzo.includes(time);
+
 // cena
 let isOpenCenaDropdown = ref(false)
 </script>
@@ -21,17 +33,24 @@ let isOpenCenaDropdown = ref(false)
         //- Pranzo
         div.mb-6
             p.mb-4 Pranzo
-            .flex.items-center.justify-between.border.rounded.py-2.px-3.mb-2(v-for="time in workHoursRestaurant.pranzo", :key="time")
+            //- TIME LIST
+            .flex.items-center.justify-between.border.rounded.py-2.px-3.mb-2(v-for="(time, index) in restaurantPranzoHours", :key="time")
                 p.leading-normal.text-grey-300 {{ time }}
-                SVGIcon.text-grey-300(svg="trash", :size="15")
-            a.flex.items-center.justify-between.border.border-dashed.border-primary-100.rounded.py-2.px-3.mb-2(v-if="showPranzoAddTime", @click="togglePranzoSelect")
+                SVGIcon.text-grey-300.cursor-pointer.hover_text-error-200(svg="trash", :size="15", @click="removeTimeSetPranzo(index)")
+            //- AGGIUNGI ORARIO
+            .flex.items-center.justify-between.border.border-dashed.border-primary-100.rounded.py-2.px-3.mb-2.cursor-pointer.hover_bg-slate-50(
+                v-if="showPranzoAddTime", @click="togglePranzoSelect")
                 p.leading-normal.text-primary-100 Aggiungi Orario
                 SVGIcon.text-primary-100(svg="plus", :size="15")
-            a.flex.items-center.justify-between.border.border-primary-200.rounded.py-2.px-3.mb-2.relative(v-if="showPranzoSelect", @click="togglePranzoDropdown")
-                p.leading-normal.text-primary-200 Seleziona Orario
-                SVGIcon.text-primary-200(svg="arrow-down", :size="15")
-                .absolute.inset-x-0.top-12.max-h-40.bg-white.rounded.shadow-lg.overflow-y-scroll(v-show="isOpenPranzoDropdown")
-                    p.text-grey-300.py-2.px-3.hover_bg-gray-100(v-for="time in workHoursAvailable.pranzo", :key="time") {{ time }}
+            //- SELECT TIME
+            .flex.items-center.justify-between.border.border-primary-100.rounded.py-2.px-3.mb-2.relative.cursor-pointer.hover_bg-slate-50(
+                v-if="showPranzoSelect", @click="togglePranzoDropdown")
+                p.leading-normal.text-primary-100 Seleziona Orario
+                SVGIcon.text-primary-100(svg="arrow-down", :size="15")
+                .absolute.inset-x-0.top-12.max-h-40.bg-white.rounded.shadow-lg.overflow-y-scroll(v-show="isPranzoDropdownOpen")
+                    p.text-grey-300.py-2.px-3.cursor-pointer(v-for="time in workHoursAvailable.pranzo", :key="time", 
+                        :class="{ 'cursor-not-allowed line-through	bg-gray-50 text-gray-200' : isTimeUsed(time), 'hover_bg-gray-100' : !isTimeUsed(time) }", 
+                        @click="addTimeSetPranzo(time)") {{ time }}
 
         //- Divider
         .h-full.border-r
