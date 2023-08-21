1b9bfc7da34afd42df936b38f8ce82475da67b8b
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-21 17:01:08 +0200

    blocco giorni calendar

--- pages/blocco-giorni.vue
+++ pages/blocco-giorni.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import restaurantWeekDaysBlocked from "@/data/db-week-days-blocked.json";
 import weekDaysAvailable from "@/data/week-days-available.json";
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction' // needed for dateClick(), to drag and create events
 
 // Define a constant for the imported data
 const blockedDaysWeekly = ref(restaurantWeekDaysBlocked);
@@ -20,6 +23,52 @@ const addOrUpdateDay = (oldDayName: string, newDayName: string, isUpdate: boolea
 const blockedDaysArrayShort = computed(() => blockedDaysWeekly.value.length < 1 ? true : false)
 // if all days are added
 const blockedDaysArrayFull = computed(() => blockedDaysWeekly.value.length > 6)
+
+//************
+// CALENDAR
+//************
+const handleEventClick = (clickInfo: any) => {
+    // if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    //     clickInfo.event.remove()
+    // }
+    if (confirm(`Sicuro di voler eliminare l'evento '${clickInfo.event.title}'?`)) {
+        clickInfo.event.remove()
+    }
+}
+const handleDateSelect = (selectInfo: any) => {
+    // let title = prompt('Please enter a new title for your event')
+    let title = prompt('Inserisci un titolo for questo evento', 'Blocco giorni')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    calendarApi.addEvent({
+        id: Math.random(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+    })
+}
+const handleEvents = (events: any) => {
+    currentEvents = events
+}
+
+let currentEvents = []
+const calendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin],
+    headerToolbar: { left: 'prev', center: 'title', right: 'next' },
+    initialView: 'dayGridMonth',
+    events: [],
+    editable: true,
+    selectable: true,
+    // selectMirror: true,
+    dayMaxEvents: true,
+    // weekends: true, // default
+    select: handleDateSelect,
+    eventClick: handleEventClick,
+    eventsSet: handleEvents
+}
 </script>
 
 
@@ -28,7 +77,6 @@ const blockedDaysArrayFull = computed(() => blockedDaysWeekly.value.length > 6)
     PageTitle(title="Blocco Giorni")
 
     .grid.gap-6.border-b(:class="['grid-cols-[2fr_1px_1fr]', {'items-center' : blockedDaysArrayShort }]")
-
         div.mt-2.mb-8
             p.text-lg.text-grey-300 Giorno di Chiusura Settimanale
             p.text-sm.text-grey-200.font-light Personalizza il giorno di chiusura settimanale del ristorante. Questa opzione ti consente di stabilire con precisione il giorno in cui il ristorante resterà chiuso ogni settimana.
@@ -43,4 +91,6 @@ const blockedDaysArrayFull = computed(() => blockedDaysWeekly.value.length > 6)
             //- Display the "empty" one, to add a new day
             DaySelection(v-if="!blockedDaysArrayFull", :restaurantWeekDaysBlocked="blockedDaysWeeklySorted", 
                 @addOrUpdateDay="addOrUpdateDay", @removeDay="removeDay", :isUpdate="false", :showTrash="false")
+
+    FullCalendar.mt-8(:options='calendarOptions')
 </template>
