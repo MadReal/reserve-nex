e5cd07b7f37ce7c826509a4da0e08b07ac76e128
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-23 19:23:16 +0200

    rename routes

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -18,23 +18,23 @@
         .mb-8.mt-4
             p.mb-3.px-3.text-xs.tracking-widest.text-grey-200 MENU
 
-            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/prenotazioni" class="")
+            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/reservations" class="")
                 SVGIcon.text-grey-100.mr-2.group-hover_text-grey-200(svg="check", :size="20")
                 p.text-sm.text-grey-200.tracking-wide Prenotazioni
-            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/riepilogo-giorno")
+            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/day-summary")
                 SVGIcon.text-grey-100.mr-2.group-hover_text-grey-200(svg="calendar", :size="20")
                 p.text-sm.text-grey-200.tracking-wide Riepilogo Giorno
-            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/gestione-orari")
+            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/edit-time")
                 SVGIcon.text-grey-100.mr-2.group-hover_text-grey-200(svg="clock", :size="20")
                 p.text-sm.text-grey-200.tracking-wide Gestione Orari
 
         .my-8
             p.mb-3.px-3.text-xs.tracking-widest.text-grey-200 GESTIONE BLOCCHI
 
-            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/blocco-giorni")
+            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/block-days")
                 SVGIcon.text-grey-100.mr-2.group-hover_text-grey-200(svg="block", :size="20")
                 p.text-sm.text-grey-200.tracking-wide Blocco Giorni
-            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/blocco-orari")
+            NuxtLink.router-link.flex.items-center.p-4.cursor-pointer.group(to="/admin/block-time")
                 SVGIcon.text-grey-100.mr-2.group-hover_text-grey-200(svg="block", :size="20")
                 p.text-sm.text-grey-200.tracking-wide Blocco Orari
 </template>
