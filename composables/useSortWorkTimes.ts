be1f5eb0ee51b0dedf57e49aaf3bf951f15c2388
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-24 16:14:37 +0200

    fixed sort times composable

--- composables/useSortWorkTimes.ts
+++ composables/useSortWorkTimes.ts
@@ -1,7 +1,12 @@
 export default function useSortWorkTimes(array: WorkTime[]): WorkTime[] {
 	return array.sort((a, b) => {
-		if (a.time === "00:00" || a.time === "00:30") return 1; // "00:00" and "00:30" should come last
-		if (b.time === "00:00" || b.time === "00:30") return -1; // "00:00" and "00:30" should come last
-		return parseInt(a.time) - parseInt(b.time);
+		const [aHour, aMinute] = a.time.split(":").map(Number);
+		const [bHour, bMinute] = b.time.split(":").map(Number);
+
+		if (aHour === 0 && (aMinute === 0 || aMinute === 30)) return 1;
+		if (bHour === 0 && (bMinute === 0 || bMinute === 30)) return -1;
+
+		if (aHour === bHour) return aMinute - bMinute;
+		else return aHour - bHour;
 	});
 }
