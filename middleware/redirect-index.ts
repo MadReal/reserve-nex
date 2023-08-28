124a2711a385da10a18ef5a883d8b492fb2d75bd
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-08-28 10:15:13 +0200

    redirect index admin

---
+++ middleware/redirect-index.ts
@@ -0,0 +1,10 @@
+import { useRoute } from "vue-router";
+
+export default defineNuxtRouteMiddleware(() => {
+	const route = useRoute();
+
+	if (/^\/admin\/?$/.test(route.path)) {
+		// return navigateTo("/admin/reservations");
+		return navigateTo("/admin/blocked-days");
+	}
+});
