111879d73e32ffe7e66f7db26fbf96a92df79a1c
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-13 19:47:25 +0200

    fix modal bg not being centered - full screen

--- components/Modal/ModalManager.vue
+++ components/Modal/ModalManager.vue
@@ -1,28 +1,43 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { useModalsStore } from '~/stores/Modals'
-import { vOnClickOutside } from '@vueuse/components'
+import { storeToRefs } from "pinia";
+import { useModalsStore } from "~/stores/Modals";
+import { vOnClickOutside } from "@vueuse/components";
 
-const storeModals = useModalsStore()
-const { activeModal } = storeToRefs(storeModals)
+const storeModals = useModalsStore();
+const { activeModal } = storeToRefs(storeModals);
 
-watch(() => activeModal.value, () => {
-	if (activeModal.value) document.body.classList.add('overflow-hidden')
-	else document.body.classList.remove('overflow-hidden')
-})
+watch(
+  () => activeModal.value,
+  () => {
+    if (activeModal.value) document.body.classList.add("overflow-hidden");
+    else document.body.classList.remove("overflow-hidden");
+  },
+);
 </script>
 
-
 <template>
-	<div class="modalManager flex items-center justify-center absolute inset-0 z-50 overflow-y-scroll bg-black/80"
-		v-if="activeModal !== null">
-		<div class="relative w-11/12 lg_w-4/12 lg_min-w-[36rem] bg-white rounded-lg border overflow-y-clip border-slate-100 py-8 px-6 md_p-12 md_pb-16"
-			v-on-click-outside="storeModals.closeModal">
-			<div class="absolute top-3 right-3 rounded-full border border-black cursor-pointer hover_bg-slate-100 p-0.5">
-				<SVGIcon class="text-black" svg="close" :size="24" @click="storeModals.closeModal()" />
-			</div>
-			<ModalRestaurant v-if="activeModal === 'restaurant'"></ModalRestaurant>
-			<ModalReservation v-else-if="activeModal === 'reservation'"></ModalReservation>
-		</div>
-	</div>
+  <div
+    class="modalManager fixed inset-0 z-50 flex items-center justify-center overflow-y-scroll bg-black/80"
+    v-if="activeModal !== null"
+  >
+    <div
+      class="relative w-11/12 overflow-y-clip rounded-lg border border-slate-100 bg-white px-6 py-8 md_p-12 md_pb-16 lg_w-4/12 lg_min-w-[36rem]"
+      v-on-click-outside="storeModals.closeModal"
+    >
+      <div
+        class="absolute right-3 top-3 cursor-pointer rounded-full border border-black p-0.5 hover_bg-slate-100"
+      >
+        <SVGIcon
+          class="text-black"
+          svg="close"
+          :size="24"
+          @click="storeModals.closeModal()"
+        />
+      </div>
+      <ModalRestaurant v-if="activeModal === 'restaurant'"></ModalRestaurant>
+      <ModalReservation
+        v-else-if="activeModal === 'reservation'"
+      ></ModalReservation>
+    </div>
+  </div>
 </template>
