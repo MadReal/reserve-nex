d06e3f03e457e52791e7bb8275d1b9ee8f07da33
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-13 08:59:29 +0200

    packages

--- components/NotificationManager.vue
+++ components/NotificationManager.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { useNotificationsStore } from '~/stores/Notifications'
-import { vOnClickOutside } from '@vueuse/components'
+import { storeToRefs } from "pinia";
+import { useNotificationsStore } from "~/stores/Notifications";
+import { vOnClickOutside } from "@vueuse/components";
 
-const storeNotifications = useNotificationsStore()
-const { activeNotification } = storeToRefs(storeNotifications)
+const storeNotifications = useNotificationsStore();
+const { activeNotification } = storeToRefs(storeNotifications);
 </script>
 
-
 <template>
-	<Transition>
-		<div class="NotificationManager z-50 shadow absolute bottom-4 inset-x-2 md_left-auto bg-green-400 text-white rounded-lg py-2 px-3 md_px-4 flex items-center md_bottom-10 md_right-7 w-auto lg_min-w-[300px] "
-			v-if="activeNotification !== null">
-			<p class="text-sm"> {{ activeNotification?.text }} </p>
-			<div class="ml-auto cursor-pointer p-1 rounded-lg hover_bg-green-500"
-				@click="storeNotifications.closeNotification()">
-				<SVGIcon class="" svg="close" :size="24" />
-			</div>
-		</div>
-	</Transition>
+  <Transition>
+    <div
+      class="NotificationManager absolute inset-x-2 bottom-4 z-50 flex w-auto items-center rounded-lg bg-green-400 px-3 py-2 text-white shadow md_bottom-10 md_left-auto md_right-7 md_px-4 lg_min-w-[300px]"
+      v-if="activeNotification !== null"
+    >
+      <p class="text-sm">{{ activeNotification?.text }}</p>
+      <div
+        class="ml-auto cursor-pointer rounded-lg p-1 hover_bg-green-500"
+        @click="storeNotifications.closeNotification()"
+      >
+        <SVGIcon class="" svg="close" :size="24" />
+      </div>
+    </div>
+  </Transition>
 </template>
-
 
 <style>
 /* we will explain what these classes do next! */
 .v-enter-active,
 .v-leave-active {
-	transition: all 0.5s ease;
+  transition: all 0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-	transform: translateX(200px);
-	opacity: 0;
+  transform: translateX(200px);
+  opacity: 0;
 }
 </style>
