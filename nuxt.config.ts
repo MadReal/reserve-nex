549000a591573b60130f2678a2d7134b18ccb312
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-14 21:26:39 +0200

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ export default defineNuxtConfig({
     head: {
       title: "Ristorante",
       charset: "utf-8",
-      // viewport: "width=device-width, initial-scale=1.0, user-scalable=no;user-scalable=0",
       viewport: "width=device-width, initial-scale=1.0, user-scalable=no",
       htmlAttrs: { lang: "it" },
       // meta: [
