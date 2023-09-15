b962b93453a8e8b47bf1ecfe61a8de06b5a4fc74
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-15 16:28:43 +0200

    fix errors with VueDatePicker

--- components/Admin/AdminReservationFilterCalendar.vue
+++ components/Admin/AdminReservationFilterCalendar.vue
@@ -18,45 +18,32 @@ const removeDate = () => {
 
 // Data Picker dropdown
 const isDropdownCalendarOpen = ref(false);
-const toggleDropdownCalendar = () =>
-  (isDropdownCalendarOpen.value = !isDropdownCalendarOpen.value);
+const toggleDropdownCalendar = () => (isDropdownCalendarOpen.value = !isDropdownCalendarOpen.value);
 const closeDropdownCalendar = () => (isDropdownCalendarOpen.value = false);
 </script>
 
 <template>
   <div
     class="relative flex cursor-pointer items-center"
+    :class="{ 'text-grey-100': !selectedDate, 'rounded-md bg-slate-50 pl-1 text-grey-200': selectedDate }"
     v-on-click-outside="closeDropdownCalendar"
   >
-    <p
-      class="px-2 py-1 text-sm leading-normal text-grey-100"
-      :class="{
-        'rounded-md bg-slate-50 text-grey-300': selectedDate,
-      }"
-      @click="toggleDropdownCalendar()"
-    >
+    <p class="px-1 py-1 text-sm leading-normal hover_text-grey-300" @click="toggleDropdownCalendar()">
       {{ selectedDate ? useDateFormatting(selectedDate) : "Seleziona data" }}
     </p>
-    <SVGIcon
-      svg="close"
-      v-if="selectedDate"
-      @click="removeDate()"
-      class="ml-1 text-grey-200 hover_text-grey-300"
-    ></SVGIcon>
+    <div class="p-1 hover_text-grey-300" @click="removeDate()" v-if="selectedDate">
+      <SVGIcon svg="close"></SVGIcon>
+    </div>
 
-    <div
-      v-show="isDropdownCalendarOpen"
-      class="absolute right-0 top-8 z-10 h-fit rounded-lg bg-white shadow-lg md_-left-3"
-    >
+    <div v-show="isDropdownCalendarOpen" class="absolute right-0 top-8 z-10 h-fit rounded-lg bg-white shadow-lg md_-left-3">
       <VueDatePicker
         v-model="selectedDate"
         locale="it"
         :month-change-on-scroll="false"
         :enable-time-picker="false"
         :clearable="true"
-        inline="inline"
-        auto-apply="auto-apply"
-        :state="true"
+        inline
+        auto-apply
         @update:model-value="setNewDate()"
       ></VueDatePicker>
     </div>
