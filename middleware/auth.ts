243a390b4abf3ff6737641402abc0b33b08a5b14
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-27 00:09:32 +0200

    added token auth fix in middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,10 @@
 export default defineNuxtRouteMiddleware(async () => {
 	const supabaseClient = useSupabaseClient();
+	const storeAuth = useAuthStore();
 
 	const { data } = await supabaseClient.auth.getSession();
 	const authToken: string | undefined = data?.session?.access_token
 
 	if (!supabaseClient || !authToken) return navigateTo("/auth/login");
+	else storeAuth.setAuthToken(authToken);
 });
