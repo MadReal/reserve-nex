2598f1efd028cbf2f4f7d1c29bc0784948a3d017
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-27 12:35:05 +0200

    cleanups

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,62 +3,42 @@ import svgLoader from "vite-svg-loader";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // https://nuxt.com/docs/guide/concepts/rendering#hybrid-rendering
 export default defineNuxtConfig({
+  devtools: { enabled: false },
   app: {
     head: {
       title: "Ristorante",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1.0, user-scalable=no",
-      htmlAttrs: { lang: "it" },
-      // meta: [
-      // 	{ name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no', },
-      // ],
+      htmlAttrs: { lang: "it" }
     },
   },
-  devtools: { enabled: false },
   routeRules: {
     // Homepage pre-rendered at build time
     "/": { prerender: true },
     // Admin dashboard renders only on client-side
     "/admin/**": { ssr: false },
     // Add cors headers on API routes
-    "/api/**": { cors: true },
+    "/api/**": { cors: true }
   },
   build: { transpile: ["@vuepic/vue-datepicker"] },
   imports: { dirs: ["stores"] },
   css: ["~/assets/css/main.sass"],
   modules: [
     "@nuxt/image",
-    "nuxt-gtag",
     "@nuxtjs/supabase",
     "@pinia-plugin-persistedstate/nuxt",
     ["@pinia/nuxt", { autoImports: ["defineStore", "acceptHMRUpdate"] }],
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: { Poppins: [300, 400, 500, 600] },
-        // preload: true,
-        download: true,
-        overwriting: false,
-        display: "swap",
-      },
-    ],
+    ["@nuxtjs/google-fonts", { families: { Poppins: [300, 400, 500, 600] }, download: true, overwriting: false, display: "swap" }],
+    "nuxt-gtag",
     'nuxt3-meta-pixel'
   ],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  postcss: { plugins: { tailwindcss: {}, autoprefixer: {} } },
   supabase: { redirect: false },
   vite: { plugins: [svgLoader()] },
-  image: {
-    format: ["webp"],
-    domains: ["avatars0.githubusercontent.com"],
-  },
+  image: { format: ["webp"], domains: ["avatars0.githubusercontent.com"] },
+  // events tracking Google Analytics, Facebook
   gtag: { id: "G-7WHW3K85WN" },
   facebook: {
-    /* module options */
     track: 'PageView',
     pixelId: '868772751506596',
     autoPageView: true,
