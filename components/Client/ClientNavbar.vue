a76cfe8dad0c2b5ff7062d6d4a360744029772bd
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-09-29 16:41:23 +0200

    init restaurant presentation

--- components/Client/ClientNavbar.vue
+++ components/Client/ClientNavbar.vue
@@ -1,81 +1,96 @@
 <script setup lang="ts">
-import { vOnClickOutside } from '@vueuse/components'
-const routeName = computed(() => useRoute().name)
+import { vOnClickOutside } from "@vueuse/components";
+const routeName = computed(() => useRoute().name);
 
-const isRouteHome = computed(() => useRoute().path === '/')
-const isNavbarTextWhite = computed(() => routeName.value === 'reservation-restaurantId')
+const hasScrolled = ref(); // obtain the reference
+const isRouteHome = computed(() => useRoute().path === "/");
+const isNavTextWhite = computed(
+  () => routeName.value === "reservation-restaurantId" || (isRouteHome.value && !hasScrolled.value),
+);
+const isNavTextBlack = computed(() => !isRouteHome.value || (isRouteHome.value && hasScrolled.value));
 
+let isMenuOpen = ref(false);
+const toggleMenu = () => (isMenuOpen.value = !isMenuOpen.value);
+const closeMenu = () => (isMenuOpen.value = false);
 
-let isMenuOpen = ref(false)
-function toggleMenu() {
-    isMenuOpen.value = !isMenuOpen.value
-}
-function closeMenu() {
-    isMenuOpen.value = false
-}
+onMounted(() => {
+  window.addEventListener("scroll", () => {
+    var curr = window.scrollY;
+
+    if (curr >= 80) hasScrolled.value = true;
+    else hasScrolled.value = false;
+  });
+});
 </script>
 
-
 <template>
-    <nav id="nav" class="sticky md_fixed inset-x-0 top-0 flex flex-row justify-between z-40 h-14 md_h-min md_bg-transparent"
-        :class="{ 'text-white': isNavbarTextWhite || isRouteHome, 'bg-white text-black': !isNavbarTextWhite }">
+  <nav
+    id="nav"
+    class="sticky inset-x-0 top-0 z-40 flex h-14 flex-row justify-between transition duration-100 md_fixed md_h-min"
+    :class="{
+      'text-white': isNavTextWhite,
+      'bg-white text-black': isNavTextBlack,
+    }"
+  >
+    <div class="p-4">
+      <div class="text-xl font-semibold">
+        <nuxt-link
+          to="/"
+          class="transition duration-200 hover_text-primary-100 focus_text-black"
+          :class="{ 'text-black': isRouteHome }"
+          >Ristorante</nuxt-link
+        >
+      </div>
+    </div>
 
-        <div class="p-4">
-            <div class="font-semibold text-xl">
-                <nuxt-link to="/" class="text-black transition duration-300 hover_text-primary-100 focus_text-black"
-                    :class="{ 'text-white': isNavbarTextWhite }">Ristorante</nuxt-link>
-            </div>
+    <!-- Nav Items Working on Tablet & Bigger Sceen -->
+    <div class="hidden flex-row justify-between p-4 text-lg font-semibold md_flex">
+      <nuxt-link to="/" class="navbar-item">Home</nuxt-link>
+      <a href="#about" class="navbar-item">Menu</a>
+      <a href="#contactus" class="navbar-item">Contatti </a>
+      <nuxt-link to="/admin" class="navbar-item-admin">Admin</nuxt-link>
+    </div>
+
+    <!-- Burger Nav Button on Mobile -->
+    <div class="md_hidden" v-on-click-outside="closeMenu">
+      <div id="nav-open" class="p-4 text-black md_hidden" @click="toggleMenu()">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-menu"
+        >
+          <line x1="3" y1="12" x2="21" y2="12"></line>
+          <line x1="3" y1="6" x2="21" y2="6"></line>
+          <line x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
+      </div>
+
+      <div class="absolute inset-x-0 top-14 z-10 border-b bg-white" v-show="isMenuOpen">
+        <div class="text-black">
+          <ul class="mb-8 mt-1">
+            <li class="py-3" @click="toggleMenu()"><nuxt-link to="/" class="navbar-item">Home</nuxt-link></li>
+            <li class="py-3" @click="toggleMenu()"><a class="navbar-item">Menu</a></li>
+            <li class="py-3" @click="toggleMenu()"><a class="navbar-item">Contatti </a></li>
+            <li class="py-3" @click="toggleMenu()"><nuxt-link to="/admin" class="navbar-item-admin">Admin</nuxt-link></li>
+          </ul>
         </div>
-
-        <!-- Nav Items Working on Tablet & Bigger Sceen -->
-        <div class="p-4 text-lg hidden md_flex flex-row justify-between font-semibold">
-            <nuxt-link to="/"
-                class="mx-4 border-b-2 border-transparent hover_border-b-2 hover_border-primary-200 transition duration-300">Home</nuxt-link>
-            <a href="#about"
-                class="mx-4 border-b-2 border-transparent hover_border-b-2 hover_border-primary-200 transition duration-300">Menu</a>
-            <a href="#contactus"
-                class="mx-4 border-b-2 border-transparent hover_border-b-2 hover_border-primary-200 transition duration-300">Contatti
-            </a>
-            <nuxt-link to="/admin"
-                class="mx-4 border-b-2 border-primary-200 hover_border-b-2 hover_border-indigo-300 transition duration-300">Admin</nuxt-link>
-        </div>
-
-        <!-- Burger Nav Button on Mobile -->
-        <div class="md_hidden" v-on-click-outside="closeMenu">
-            <div id="nav-open" class="p-4 md_hidden text-black" @click="toggleMenu()">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="feather feather-menu">
-                    <line x1="3" y1="12" x2="21" y2="12"></line>
-                    <line x1="3" y1="6" x2="21" y2="6"></line>
-                    <line x1="3" y1="18" x2="21" y2="18"></line>
-                </svg>
-            </div>
-
-            <div class="absolute bg-white z-10 inset-x-0 top-14 border-b" v-show="isMenuOpen">
-                <div class="text-black">
-                    <ul class="mt-1 mb-8">
-                        <li class="py-3" @click="toggleMenu()">
-                            <nuxt-link to="/"
-                                class="mx-4 border-b-2 border-transparent hover_border-b-2 hover_border-primary-200 transition duration-300">Home</nuxt-link>
-                        </li>
-                        <li class="py-3" @click="toggleMenu()">
-                            <a class="mx-4 border-b-2 border-transparent hover_border-b-2 hover_border-primary-200 transition
-                                duration-300">Menu</a>
-                        </li>
-                        <li class="py-3" @click="toggleMenu()">
-                            <a class="mx-4 border-b-2 border-transparent hover_border-b-2 hover_border-primary-200 transition
-                                duration-300">Contatti
-                            </a>
-                        </li>
-                        <li class="py-3" @click="toggleMenu()">
-                            <nuxt-link to="/admin"
-                                class="mx-4 border-b-2 border-primary-200 hover_border-b-2 hover_border-indigo-300 transition duration-300">Admin</nuxt-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
+
+<style lang="sass">
+.navbar-default
+    @apply mx-4 border-b-2 transition duration-200 hover_border-b-2
+.navbar-item
+    @apply navbar-default border-transparent hover_border-primary-200
+.navbar-item-admin
+    @apply navbar-default border-primary-200 hover_border-indigo-300
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,40 +5,36 @@ useHead({ title: "Ristorante" });
 const img1 = ref<string>();
 const img2 = ref<string>();
 const img3 = ref<string>();
-
 const isMouseOver = ref(false); // Track mouse hover state
-
 // Define maximum movement values (adjust these values according to your preference)
 const maxMoveX = 5;
 const maxMoveY = 2;
-
 function onMousemove(e: MouseEvent) {
   const container = e.currentTarget as HTMLDivElement;
-
   // Calculate the percentage of mouse position within the container
   const mouseX = (e.clientX - container.offsetLeft) / container.clientWidth;
   const mouseY = (e.clientY - container.offsetTop) / container.clientHeight;
-
   // Calculate the movement values within the specified range
   const moveX = mouseX * 2 * maxMoveX - maxMoveX;
   const moveY = mouseY * 2 * maxMoveY - maxMoveY;
-
   // Apply the movement to the images
   img1.value = `translate(${moveX * 3.3}px, ${moveY * 0.6}px)`;
   img2.value = `translate(${moveX * 1.5}px, ${moveY * 2.8}px)`;
   img3.value = `translate(${moveX * 0.5}px, ${moveY * 4.1}px)`;
 }
-
 function onMouseleave(e: MouseEvent) {
   isMouseOver.value = true;
   // Apply the movement to the images
   img1.value = `translate(0px, 0px)`;
   img2.value = `translate(0px, 0px)`;
   img3.value = `translate(0px, 0px)`;
-  setTimeout(() => {
-    isMouseOver.value = false;
-  }, 700);
+  setTimeout(() => (isMouseOver.value = false), 700);
 }
+
+// const imgStyle = (picPosition: "left" | "right") =>
+//   picPosition === "left"
+//     ? `transform: perspective(400px) rotate3d(1, -1, 0, -8deg)`
+//     : `transform: perspective(400px) rotate3d(1, -1, 0, 8deg)`;
 </script>
 
 <template>
@@ -57,54 +53,84 @@ function onMouseleave(e: MouseEvent) {
           <NuxtLink
             class="mr-3 inline-flex justify-center rounded bg-primary-100 px-5 py-3 text-center text-base font-medium uppercase text-white hover_bg-primary-200"
             to="/reservation"
-            >Prenota</NuxtLink
-          >
+            >Prenota
+          </NuxtLink>
           <NuxtLink
             class="inline-flex justify-center rounded border border-primary-100 px-5 py-3 text-center text-base font-medium uppercase text-primary-100 hover_bg-primary-100/10"
             to="/admin"
-            >Vedi Admin</NuxtLink
-          >
+            >Vedi Admin
+          </NuxtLink>
         </div>
 
         <div class="relative hidden h-full md_block md_basis-1/2" @mousemove="onMousemove" @mouseleave="onMouseleave">
           <NuxtImg
             :style="{ transform: img1 }"
-            fit="cover"
             loading="lazy"
             placeholder
             class="absolute -left-2 -top-20 rounded-sm drop-shadow-lg transition-transform duration-100"
             :class="{ 'duration-700': isMouseOver }"
             src="/images/header-plate3.webp"
             width="288px"
-          >
-          </NuxtImg>
+          />
           <NuxtImg
             :style="{ transform: img2 }"
-            fit="cover"
             loading="lazy"
             placeholder
             class="absolute right-0 top-20 rounded-sm drop-shadow-lg transition-transform duration-100"
             :class="{ 'duration-700': isMouseOver }"
             src="/images/header-plate2.webp"
             width="330px"
-          >
-          </NuxtImg>
+          />
           <NuxtImg
             :style="{ transform: img3 }"
-            fit="cover"
             loading="lazy"
             placeholder
             class="absolute -bottom-20 left-32 rounded-sm drop-shadow-lg transition-transform duration-100"
             :class="{ 'duration-700': isMouseOver }"
             src="/images/header-plate1.webp"
             width="400px"
-          >
-          </NuxtImg>
+          />
         </div>
       </div>
       <div
         class="absolute inset-y-0 right-0 z-0 h-full w-full bg-primary-200 opacity-20 [clip-path:polygon(100%_0%,40%_100%,0%_100%,120%_-10%)] [shape-outside:polygon(100%_0%,40%_100%,0%_100%,120%_-10%)] md_w-3/5 md_bg-black md_opacity-100 md_[clip-path:polygon(100%_0%,100%_100%,40%_100%,0%_0%)] md_[shape-outside:polygon(100%_0%,100%_100%,50%_100%,0%_0%)]"
       ></div>
+    </section>
+
+    <section class="bg-slate-50 px-4 py-28">
+      <div class="mx-auto max-w-screen-md text-center">
+        <h2 class="text-4xl font-semibold text-black">Come funziona il sito</h2>
+        <p class="text-grey-200">
+          Commodo ea voluptate est laborum nisi nisi deserunt. Nulla commodo ipsum adipisicing consectetur dolore consectetur.
+          Labore in magna adipisicing duis anim sunt ullamco nostrud.
+        </p>
+      </div>
+
+      <div class="mx-auto my-24 flex max-w-screen-lg gap-8">
+        <div class="basis-3/6">
+          <NuxtImg class="drop-shadow-lg" loading="lazy" placeholder src="/images/admin-dashboard1.webp" width="500px" />
+        </div>
+        <div class="mt-14 basis-3/6">
+          <h2 class="text-xl font-semibold text-black">Come funziona il sito</h2>
+          <p class="text-grey-200">
+            Commodo ea voluptate est laborum nisi nisi deserunt. Nulla commodo ipsum adipisicing consectetur dolore consectetur.
+            Labore in magna adipisicing duis anim sunt ullamco nostrud.
+          </p>
+        </div>
+      </div>
+
+      <div class="mx-auto my-24 flex max-w-screen-lg gap-8">
+        <div class="mt-14 basis-3/6">
+          <h2 class="text-xl font-semibold text-black">Come funziona il sito</h2>
+          <p class="text-grey-200">
+            Commodo ea voluptate est laborum nisi nisi deserunt. Nulla commodo ipsum adipisicing consectetur dolore consectetur.
+            Labore in magna adipisicing duis anim sunt ullamco nostrud.
+          </p>
+        </div>
+        <div class="basis-3/6">
+          <NuxtImg class="drop-shadow-lg" loading="lazy" placeholder src="/images/admin-dashboard2.webp" width="500px" />
+        </div>
+      </div>
     </section>
   </div>
 </template>
