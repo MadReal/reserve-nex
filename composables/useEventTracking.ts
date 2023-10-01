c63711ba2e5a6d03e56f2f7254cd3bfe485d8673
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-10-01 17:28:03 +0200

    cleanup events code

--- composables/useEventTracking.ts
+++ composables/useEventTracking.ts
@@ -9,25 +9,10 @@ import { useNuxtApp } from '#app';
 
 export function useEventTracking() {
 
-	// function trackFacebookEvent(eventName: string, eventParams?: any) {
-	// 	const nuxtApp = useNuxtApp()
-	// 	// @ts-ignore
-	// 	nuxtApp.$fb.track(eventName, eventParams)
-	// }
-
-	// function trackGoogleEvent(eventName: string, eventParams?: any) {
-	// 	const { gtag } = useGtag()
-
-	// 	gtag('event', 'screen_view', {
-	// 		app_name: 'My App',
-	// 		screen_name: 'Home'
-	// 	})
-	// }
-
 	function trackEventReservationStarted(restaurantName: string) {
 		const nuxtApp = useNuxtApp()
 		// @ts-ignore
-		nuxtApp.$fb.track("InitiateCheckout", { 'restaurant': restaurantName, content_name: restaurantName })
+		nuxtApp.$fb.track("InitiateCheckout", { content_name: restaurantName })
 
 		const { gtag } = useGtag()
 		gtag("event", "reservation_started", {
@@ -39,7 +24,7 @@ export function useEventTracking() {
 	function trackEventReservationFinished(restaurantName: string, value: number, reservationId: string) {
 		const nuxtApp = useNuxtApp()
 		// @ts-ignore
-		nuxtApp.$fb.track("Schedule", { 'restaurant': restaurantName, content_name: restaurantName, currency: "EUR", value })
+		nuxtApp.$fb.track("Schedule", { content_name: restaurantName, currency: "EUR", value })
 
 		const { gtag } = useGtag()
 		gtag("event", "reservation_finished", {
@@ -55,17 +40,5 @@ export function useEventTracking() {
 		});
 	}
 
-	// return { trackFacebookEvent, trackGoogleEvent, trackEventReservationStarted, trackEventReservationFinished };
 	return { trackEventReservationStarted, trackEventReservationFinished };
 }
-
-// gtag("event", "reservation_started", {
-// 	event_category: "reservation",
-// 	event_action: "started",
-// });
-
-// gtag("event", "reservation_sent", {
-// 	event_category: "reservation",
-// 	event_action: "finished",
-// 	value: clientReservationValueMinusDiscount,
-// });
