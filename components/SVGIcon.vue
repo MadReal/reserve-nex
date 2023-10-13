2837876d86b08c8f12e6771b6d8b594a50396bd2
Giacomo Galletti <giacomogalletti95@gmail.com>
2023-10-13 09:28:37 +0200

    svg icon

--- components/SVGIcon.vue
+++ components/SVGIcon.vue
@@ -1,29 +1,28 @@
 <template lang="pug">
-//- component(:is="dynamicSvg", v-if="dynamicSvg", :class="`text-${color}`", :width="size", :height="size")
 component(:is="dynamicSvg", v-if="dynamicSvg", :width="size", :height="size")
 </template>
 
 <script setup lang="ts">
 interface IconComponentProps {
-    svg: string
-    color?: string
-    size?: number
+  svg: string;
+  color?: string;
+  size?: number;
 }
 
 const props = withDefaults(defineProps<IconComponentProps>(), {
-    color: 'grey-100',
-    size: 20
-})
+  color: "grey-100",
+  size: 20,
+});
 
-let dynamicSvg = ref(null)
-
-onMounted(async () => {
-    dynamicSvg.value = await importSvg(props.svg)
-})
-
-watch(() => props.svg, async (svgText) => { dynamicSvg.value = await importSvg(svgText) })
+let dynamicSvg = ref(null);
 
 const importSvg = async (svgText: string) => {
-    if (svgText) return await import(`~/assets/icons/${svgText}.svg`)
-}
+  if (svgText) return await import(`~/assets/icons/${svgText}.svg`);
+};
+
+onMounted(async () => (dynamicSvg.value = await importSvg(props.svg)));
+watch(
+  () => props.svg,
+  async (svgText) => (dynamicSvg.value = await importSvg(svgText)),
+);
 </script>
